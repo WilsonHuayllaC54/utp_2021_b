3749ac8cde64ee7be031a6efae1c2e2937600efa
WilsonHuayllaC54 <89079293+WilsonHuayllaC54@users.noreply.github.com>
2021-11-02 11:47:32 -0500

    Add files via upload

---
+++ Farmacia/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Sistema de Inventario Farmacéutico</title>
+	
+    
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    
+  </head>
+
+  <body style="background:#192632 " >
+  <br/>
+  <br/>    
+      <div class="container" >
+
+      <div id="boom" >
+      <h1>FARMASUR </h1>
+      </div>
+        
+
+  <div id="login-form">
+
+    <h3>Iniciar sesión
+</h3>
+
+    <fieldset>
+
+      <form id="form-login">
+        <input type="text" autofocus id="un" placeholder="Usuario" required autocomplete="off"> 
+
+        <input type="password" id="up" placeholder="Contraseña" required autocomplete="off">  
+
+        <input type="submit" name="log" value="Ingresar">
+
+        
+      </form>
+
+    </fieldset>
+
+  </div> <!-- end login-form -->
+
+</div>
+
+        <div class="fondo">
+          <div class="reflector"></div>
+          <div class="hair"></div>
+          <div class="brazos"></div>
+          <div class="brazos3"></div>
+          <div class="dj">
+            
+            <div class="auri"></div>
+            <div class="flequi"></div>
+            <div class="boca"></div>
+          </div>
+          <div class="cuadrado"></div>
+          <div class="cables"></div>
+          <div class="disk"></div>
+          <div class="disk3"></div>
+          <div class="disk4"></div>
+          <div class="disk5"></div>
+          <div class="cuad"><div class="circ"></div><div class="circ3"></div></div>
+          <div class="cuad3"><div class="circ"></div><div class="circ3"></div></div>
+        
+        </div>
+    
+
+    
+<script type="text/javascript" src="assets/js/jquery-1.12.3.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/regis.js"></script>
+
+    
+  </body>
+</html>
+
+
+
+ 
